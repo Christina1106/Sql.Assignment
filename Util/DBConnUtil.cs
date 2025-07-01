67b18ccae863b123a8f63a80364bb7d329d009c9
Christina1106 <161043650+Christina1106@users.noreply.github.com>
2025-07-01 10:58:17 +0530

    Update DBConnUtil.cs

--- Util/DBConnUtil.cs
+++ Util/DBConnUtil.cs
@@ -1,12 +1,30 @@
-﻿using System;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SISAPP.assessment.Util
 {
-    internal class DBConnUtil
+
+    public static class DBConnUtil
     {
+        // Method to return a SqlConnection using appsettings.json
+        public static SqlConnection GetConnection()
+        {
+            // Build the configuration object to read appsettings.json
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            // Read the connection string named "CrimeDB"
+            string connectionString = config.GetConnectionString("SISDB");
+
+            // Return a new SqlConnection
+            return new SqlConnection(connectionString);
+        }
     }
 }
